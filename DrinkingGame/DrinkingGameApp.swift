e8cbd76190589287851a258a0b340c387c3fee31
tudor-manea <tudor.manea03@gmail.com>
2025-02-18 15:18:07 +0000

    Initial Commit

---
+++ DrinkingGame/DrinkingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkingGameApp.swift
+//  DrinkingGame
+//
+//  Created by Tudor Manea on 18/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct DrinkingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DrinkingGameTests/DrinkingGameTests.swift
@@ -0,0 +1,16 @@
+//
+//  DrinkingGameTests.swift
+//  DrinkingGameTests
+//
+//  Created by Tudor Manea on 18/02/2025.
+//
+
+import Testing
+
+struct DrinkingGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
